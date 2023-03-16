5892f457676c988aeb5ab314e16f036b1cab9998
TanakaTakeshikun <maru1679@icloud.com>
2023-03-16 21:19:29 +0900

    refactor:ポート指定を可能に変更

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-const express = require('express'); 
-const pino = require('express-pino-logger')(); 
+const express = require('express');
+const pino = require('express-pino-logger')();
+const quizlist = require(".././helpers/quiz.json")
+const port = process.env.serverport || 8080;
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(pino);
 
-const app = express(); app.use(express.urlencoded({ extended: false })); 
-app.use(pino); 
+app.get('/api/', (req, res) => {
+    const id = req.query.id || '1';
+    res.setHeader('Content-Type', 'application/json');
+    res.json(quizlist[id])
+});
 
-app.get('/api/', (req, res) => { 
-    const name = req.query.name || 'World'; 
-    res.setHeader('Content-Type', 'application/json'); 
-    res.send(JSON.stringify({ greeting: `Hello ${name}!` })); 
-}); 
-
-app.listen(3001, () => console.log('Express server is running on localhost:3001') );
+app.listen(port, () => console.log(`APIサーバーの構築完了\nlocalhost:${port}/api/`));
